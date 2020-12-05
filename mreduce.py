f051f44ea85e75f135faa17eb1a3b0c5d636d6de
Favian Flores <floresf1997@gmail.com>
2020-12-04 20:40:53 -0500

    cleaned up code

--- mreduce.py
+++ mreduce.py
@@ -3,27 +3,19 @@ from pyspark.conf import SparkConf
 from operator import add
 from math import log
 
-
-def main():
-    sc = SparkContext("local", "ttr").getOrCreate()
+def tfidf(sc, data_path):
     tf_rdd = sc.emptyRDD()
     idf_rdd = sc.emptyRDD()
     data = []
 
-    # spark config to avoid manual assignment of mem
-    conf=SparkConf()\
-    .setExecutorEnv("spark.executor.memory","4g")\
-    .setExecutorEnv("spark.driver.memory","4g")
-
     #reads in file as list of lines
-    with open('data/input_test.txt', 'r') as docs:
+    with open(data_path, 'r') as docs:
         data = docs.readlines()
 
     total_docs = len(data)
 
-    
     for line in data:
-        #loads data into rdd one line('document') at a timea and flatMap separates it into individual words
+        #loads data into rdd one document at a time and separates it into individual words
         temp_tf_rdd = sc.parallelize([line]).flatMap(lambda a: a.split(" "))
 
         #takes the first element of the line (i think the first element is a sort of id, so im 
@@ -31,7 +23,7 @@ def main():
         tf_rdd_id = temp_tf_rdd.take(1)
 
         #filters out elements such as ' ', '\n', and the first id element
-        #then maps each remaining element to a key-value pair (a, 1)
+        #then maps each remaining element(a) to key-value pair (a, 1)
         temp_tf_rdd = temp_tf_rdd.filter(lambda a: (len(a) > 0) and (
             a != '\n') and (a != tf_rdd_id[0])).map(lambda a: (a, 1))
         
@@ -46,7 +38,7 @@ def main():
         tf_rdd = tf_rdd.union(temp_tf_rdd)
 
         #takes only the keys from temp_tf_rdd and creates a new RDD
-        #then maps each of those keys to a key-value pair (a,1)
+        #then maps each of those keys(a) to a key-value pair (a,1)
         temp_idf_rdd = temp_tf_rdd.keys().map(lambda a: (a, 1))
 
         #after each iteration, it unites the current line with idf_rdd which contains the entire file
@@ -58,23 +50,56 @@ def main():
                                                  (log(total_docs / a, 10)))
 
     #joins tf_rdd and idf_rdd by key and maps each value(x[0], x[1]) to (x[0] * x[1])
-    #tfidf_rdd contains final tfidf calucation for each word per line('document').
+    #tfidf_rdd contains final tfidf calucation for each word per document.
     tfidf_rdd = tf_rdd.join(idf_rdd).mapValues(lambda x: x[0] * x[1])
 
+    return tfidf_rdd
+
+def similarity(sc, tfid_rdd, query):
+    return [1,2,3]
+
+def main():
+    #spark config options
+    conf=SparkConf()\
+    .setMaster("local")\
+    .setAppName("ttr")\
+    .setExecutorEnv("spark.executor.memory","4g")\
+    .setExecutorEnv("spark.driver.memory","4g")
+    sc = SparkContext(conf=conf).getOrCreate()
+    
+    data_path = "data/input_test.txt"
+    tfidf_rdd = tfidf(sc, data_path)
+
+    """
+    done = False
+    while not done:
+        print("\nEnter a term to see its similarity to all terms in the TF-IDF matrix.")
+        query = input("\nTerm: ")
+        output = similarity(sc, tfidf_rdd, query)
+        output = [2]
+        if len(output) == 0:
+            print("\nTerm not found, try again!")
+            continue
+        print(f"\nSimilarity scores for {query}: (term, score)")
+        for a in output:
+            print(a)
+        while True:
+            print("\nWould you like to try another term?")
+            answer = input("Enter 'y' for yes or 'n' for no: ")
+            if answer == 'n':
+                done = True
+                break
+            elif answer == 'y':
+                break
+            else:
+                print("try again!")
+    """
     #Test RDDs
-    # idf_rdd is the reduced output of the corpus count
-
-    rdd_result =idf_rdd.collect()
-    for (word, count) in rdd_result:
-        print("%s: %i" % (word, count))
-
     #tf_rdd.saveAsTextFile('data/tf-out')
     #idf_rdd.saveAsTextFile('data/idf-out')
-    tfidf_rdd.saveAsTextFile('data/tfidf-out')
+    #tfidf_rdd.saveAsTextFile('data/tfidf-out')
 
     sc.stop()
 
-
 if __name__ == "__main__":
-    #print("running program")
     main()

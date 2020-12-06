08e9e6dfd00e21b70158b674c6ed27095c7cfd4f
Favian Flores <floresf1997@gmail.com>
2020-12-06 11:26:25 -0500

    Performance improvements for tfidf function

--- mreduce.py
+++ mreduce.py
@@ -2,75 +2,77 @@ from pyspark import SparkContext
 from pyspark.conf import SparkConf
 from operator import add
 from math import log
+import time
 
-def to_list(a):
+def c1(a):
     return [a]
-def append(a, b):
+def c2(a, b):
     a.append(b)
     return a
-def extend(a, b):
+def c3(a, b):
     a.extend(b)
     return a
 
+def f1(a):
+    a = a.split(' ')
+    b = []
+    for term in a:
+        if term == a[0]:
+            continue
+        b.append((term, a[0]))
+    return b
+
+#previous avg run time: 20 seconds
+#current avg run time: 3 seconds
 def tfidf(sc, data_path):
-    tf_rdd = sc.emptyRDD()
-    idf_rdd = sc.emptyRDD()
-    tfidf_rdd = sc.emptyRDD()
-    data = []
+    #imports data
+    tfidf_rdd = sc.textFile(data_path)
+    tfidf_rdd.cache()
 
-    #reads in file as list of lines
-    with open(data_path, 'r') as docs:
-        data = docs.readlines()
+    #takes count of total documents
+    total_docs = tfidf_rdd.count()
 
-    total_docs = len(data)
+    #maps data into (term, doc_id) and filters empty characters
+    tfidf_rdd = tfidf_rdd.flatMap(f1).filter(lambda a: a[0] != ' ' and a[0] != '\n' and a[0] != '')
+    tfidf_rdd.cache()
 
-    for line in data:
-        #loads data into rdd one document at a time and separates it into individual words
-        temp_tf_rdd = sc.parallelize([line]).flatMap(lambda a: a.split(" "))
+    #combines data into (term, [doc_id, doc_id2...]) and maps it to (term, idf)
+    idf_rdd = tfidf_rdd.combineByKey(c1, c2, c3)
+    idf_rdd = idf_rdd.map(lambda a: (a[0], log(total_docs/len(list(set(a[1])))), 10))
+    idf_rdd.cache()
 
-        #takes the first element(doc_id)
-        doc_id = temp_tf_rdd.take(1)
+    #maps data to ((term, doc_id), 1) and reduces it to count term frequency
+    #per document
+    tf_rdd = tfidf_rdd.map(lambda a: ((a[0], a[1]), 1))
+    tf_rdd = tf_rdd.reduceByKey(add)
+    tf_rdd.cache()
 
-        #filters out elements ' ', '\n', and doc_id
-        #then maps each remaining element(term) to key-value pair (term, 1)
-        temp_tf_rdd = temp_tf_rdd.filter(lambda a: (len(a) > 0) and (a != '\n') \
-                                    and (a != doc_id[0])).map(lambda a: (a,1))
-        
-        #takes count of the words per line
-        total_words = temp_tf_rdd.count()
+    #maps data to (doc_id, 1) and reduces it to count words per document
+    temp_tf_rdd = tfidf_rdd.map(lambda a: (a[1],1))
+    temp_tf_rdd = temp_tf_rdd.reduceByKey(add)
+    temp_tf_rdd.cache()
 
-        #reduceByKey gets a count of each word per line (termcount)
-        #then maps each value to (termcount / total_words) which solves the tf portion.
-        temp_tf_rdd = temp_tf_rdd.reduceByKey(add).mapValues(lambda a: (a / total_words))
+    #maps data to (doc_id,(word, term)) and joins it with temp_tf_rdd
+    #contains word count per document
+    tf_rdd = tf_rdd.map(lambda a: (a[0][1],(a[0][0], a[1])))
+    tf_rdd = tf_rdd.join(temp_tf_rdd)
 
-        #takes the keys of temp_tf_rdd and maps each key(term) to key-value pair (term, doc_id)
-        temp_idf_rdd = temp_tf_rdd.keys().map(lambda a: (a,doc_id[0]))
+    #maps data to (word,(doc_id, tf))
+    tf_rdd = tf_rdd.map(lambda a: (a[1][0][0], (a[0],a[1][0][1]/a[1][1])))
+    tf_rdd.cache()
 
-        #after each iteration, it unites the current line with idf_rdd which contains the entire file
-        idf_rdd = idf_rdd.union(temp_idf_rdd)
+    #joins tf_rdd and idf_rdd
+    tfidf_rdd = tf_rdd.join(idf_rdd)
 
-        #joins tf and idf rdds and maps them to (term,(doc_id, tf))
-        temp_tfidf_rdd = temp_idf_rdd.join(temp_tf_rdd)
-
-        #after each iteration, it unites the current line with tfidf_rdd which contains the entire file
-        tfidf_rdd = tfidf_rdd.union(temp_tfidf_rdd)
-
-
-    #combines all values into a list by its key(term) to end up with key-value pair (term, [doc_id, doc_id2...])
-    #then takes count of how many documents each word appears and calculates idf value (term, idf)
-    idf_rdd = idf_rdd.combineByKey(to_list, append, extend).map(lambda a: (a[0],log(total_docs/len(a[1]),10)))
-
-    #joins tfidf and idf rdds and maps them to (term, (doc_id, tf), idf)
-    tfidf_rdd = tfidf_rdd.join(idf_rdd)
-
-    #does final calculation for tfidf and maps it to (term, (doc_id, tfidf))
-    #then combines by key to final output (term, [(doc_id, tfidf), (doc_id2, tfidf2)...])
-    tfidf_rdd = tfidf_rdd.mapValues(lambda a: (a[0][0], a[0][1]*a[1])).combineByKey(to_list, append, extend)
+    #maps data to (word, (doc_id, tfidf)) and then combines it to
+    #(word, [(doc_id, tfidf), (doc_id2, tfidf2)...])
+    tfidf_rdd = tfidf_rdd.mapValues(lambda a: (a[0][0], a[0][1]*a[1]))
+    tfidf_rdd = tfidf_rdd.combineByKey(c1, c2, c3)
 
     #Test RDDs
     #tf_rdd.saveAsTextFile('data/tf-out')
     #idf_rdd.saveAsTextFile('data/idf-out')
-    tfidf_rdd.saveAsTextFile('data/tfidf-out')
+    #tfidf_rdd.saveAsTextFile('data/tfidf-out')
 
     return tfidf_rdd
 
@@ -88,7 +90,11 @@ def main():
     sc = SparkContext(conf=conf).getOrCreate()
     
     data_path = "data/input_test.txt"
+
+    #lines 95 and 97 are just to test run time, they can be deleted
+    start_time = time.time()
     tfidf_rdd = tfidf(sc, data_path)
+    print ("\nExecution time: %s seconds" % (time.time()-start_time))
 
     #Interface is complete, you can uncomment it once your function is done
     """

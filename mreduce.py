6f658666ce02a0c233fe366981bb8897a68d9594
Favian Flores <floresf1997@gmail.com>
2020-12-07 17:13:01 -0500

    modified term filter

--- mreduce.py
+++ mreduce.py
@@ -40,30 +40,31 @@ def f2(a):
 #computes tf-idf matrix
 def tfidf(sc, data_path):
     #imports data and caches to memory
-    tfidf_rdd = sc.textFile(data_path)
-    tfidf_rdd.cache()
+    tfidf_rdd = sc.textFile(data_path).cache()
 
     #takes count of total documents
     total_docs = tfidf_rdd.count()
 
-    #maps data into (term, doc_id), filters empty characters, and caches to memory
-    tfidf_rdd = tfidf_rdd.flatMap(f1).filter(lambda a: a[0] != ' ' and a[0] != '\n' and a[0] != '')
-    tfidf_rdd.cache()
+    #maps data into (term, doc_id) and caches to memory
+    tfidf_rdd = tfidf_rdd.flatMap(f1).cache()
+
+    #filters empty characters and caches to memory
+    temp_tf_rdd = tfidf_rdd.filter(lambda a: a[0] != '' and a[0] != ' ' and a[0] != '\n').cache()
+    
+    #filters any unused terms and caches to memory
+    tfidf_rdd = tfidf_rdd.filter(lambda a: a[0].startswith('dis_') or a[0].startswith('gene_')).cache()
 
     #combines data into (term, [doc_id, doc_id2...]), maps it to (term, idf), and caches to memory
     idf_rdd = tfidf_rdd.combineByKey(c1, c2, c3)
-    idf_rdd = idf_rdd.map(lambda a: (a[0], log(total_docs/len(set(a[1])), 10)))
-    idf_rdd.cache()
+    idf_rdd = idf_rdd.map(lambda a: (a[0], log(total_docs/len(set(a[1])), 10))).cache()
 
-    #maps data to ((term, doc_id), 1), reduces it to count term frequency, and caches to memory
+    #maps data to ((term, doc_id), 1), reduces it to compute term count per document, and caches to memory
     tf_rdd = tfidf_rdd.map(lambda a: ((a[0], a[1]), 1))
-    tf_rdd = tf_rdd.reduceByKey(add)
-    tf_rdd.cache()
+    tf_rdd = tf_rdd.reduceByKey(add).cache()
 
     #maps data to (doc_id, 1), reduces it to count words per document, and caches to memory
-    temp_tf_rdd = tfidf_rdd.map(lambda a: (a[1],1))
-    temp_tf_rdd = temp_tf_rdd.reduceByKey(add)
-    temp_tf_rdd.cache()
+    temp_tf_rdd = temp_tf_rdd.map(lambda a: (a[1],1))
+    temp_tf_rdd = temp_tf_rdd.reduceByKey(add).cache()
 
     #maps data to (doc_id,(word, term)) and joins it with temp_tf_rdd
     #which contains word count per document
@@ -71,16 +72,14 @@ def tfidf(sc, data_path):
     tf_rdd = tf_rdd.join(temp_tf_rdd)
 
     #maps data to (word,(doc_id, tf))
-    tf_rdd = tf_rdd.map(lambda a: (a[1][0][0], (a[0],a[1][0][1]/a[1][1])))
-    tf_rdd.cache()
+    tf_rdd = tf_rdd.map(lambda a: (a[1][0][0], (a[0],a[1][0][1]/a[1][1]))).cache()
 
     #joins tf_rdd and idf_rdd
     tfidf_rdd = tf_rdd.join(idf_rdd)
 
     #maps data to (word, (doc_id, tfidf)), combines it to
     #(word, [(doc_id, tfidf), (doc_id2, tfidf2)...]), and caches to memory
-    tfidf_rdd = tfidf_rdd.mapValues(lambda a: (a[0][0], a[0][1]*a[1])).combineByKey(c1, c2, c3)
-    tfidf_rdd.cache()
+    tfidf_rdd = tfidf_rdd.mapValues(lambda a: (a[0][0], a[0][1]*a[1])).combineByKey(c1, c2, c3).cache()
 
     return tfidf_rdd
 
@@ -92,12 +91,11 @@ def similarity(sc, tfidf_rdd, query):
         return output
     
     #filters out query term from matrix and caches matrix
-    tfidf_rdd = tfidf_rdd.filter(lambda a: a[0] != query).mapValues(lambda a: (a, output[0][1]))
-    tfidf_rdd.cache()
+    tfidf_rdd = tfidf_rdd.filter(lambda a: a[0] != query).mapValues(lambda a: (a, output[0][1])).cache()
 
     #calculates similarity, filters out 0 scores, sorts in descending order, and caches to memory
-    similarity_rdd = tfidf_rdd.mapValues(f2).filter(lambda a: a[1] != 0).sortBy(lambda a: a[1], False)
-    similarity_rdd.cache()
+    similarity_rdd = tfidf_rdd.mapValues(f2).filter(lambda a: a[1] != 0).sortBy(lambda a: a[1], False)\
+        .cache()
 
     return (similarity_rdd.collect())
 
@@ -113,10 +111,11 @@ def main():
     sc = SparkContext(conf=conf).getOrCreate()
     
     #data location
-    data_path = "data/input_test.txt"
+    data_path = "data/project2_demo.txt"
 
     #compute tf-idf matrix
     tfidf_rdd = tfidf(sc, data_path)
+    tfidf_rdd.cache()
 
     #clears screen of spark context startup notifications
     os.system('clear')
